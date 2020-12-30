0125cc3b08a1fea5a534cc6c01479b7ba1d793d1
dika <dikalaksana>
2020-12-30 09:06:46 +0800

    Change url to production

--- src/config/redux/reducer/index.js
+++ src/config/redux/reducer/index.js
@@ -2,7 +2,7 @@ const initialState = {
     project:[],
     isLoading:false,
     projectDetail:[],
-    serverURL:"http://localhost:8000/",
+    serverURL:"http://ec2-18-224-6-136.us-east-2.compute.amazonaws.com:8000/",
     s3url:"https://tinker-resource.s3.us-east-2.amazonaws.com/"
 }
 
