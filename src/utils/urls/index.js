f5cfc650b8e344c673c8bf6e352d1f70134524a1
dika <dikalaksana>
2021-01-21 23:40:42 +0800

    Change server url

--- src/utils/urls/index.js
+++ src/utils/urls/index.js
@@ -1,6 +1,6 @@
 
 
 export const url = {
-    baseUrl:"http://ec2-18-224-6-136.us-east-2.compute.amazonaws.com:8000/",
+    baseUrl:"http://ec2-3-22-222-126.us-east-2.compute.amazonaws.com:8000/",
     s3url:"https://tinker-resource.s3.us-east-2.amazonaws.com/"
 }
