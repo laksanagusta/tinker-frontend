519e0d5cd7b13111eb9481de2e69549d098159c5
dika <dikalaksana22@gmail.com>
2021-08-16 18:34:27 +0800

    Change server url

--- src/utils/urls/index.js
+++ src/utils/urls/index.js
@@ -1,6 +1,6 @@
 
 
 export const url = {
-    baseUrl:"http://ec2-13-250-113-162.ap-southeast-1.compute.amazonaws.com:8000//",
+    baseUrl:"http://ec2-13-250-113-162.ap-southeast-1.compute.amazonaws.com:8000/",
     s3url:"https://dika-portfolio.s3.ap-southeast-1.amazonaws.com/"
 }
